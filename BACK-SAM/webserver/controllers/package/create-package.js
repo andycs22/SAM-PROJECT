0227d1b9c03dca2888ab7b02a88d8c46ef79bfe3
santicoru <sansan_83@hotmail.es>
2020-01-01 21:38:34 +0100

    modificacion del create. De momento esta funciona simple

--- BACK-SAM/webserver/controllers/package/create-package.js
+++ BACK-SAM/webserver/controllers/package/create-package.js
@@ -5,20 +5,14 @@ const Joi = require('@hapi/joi')
 const mysqlPool = require('../../../database/mysql-pool');
 
 async function validateSchema(payload) {
-    const schemaPackage = Joi.object({
+    const schema = Joi.object({
         date_begin: Joi.date().format('YYYY-MM-DD').utc(),
         date_end: Joi.date().format('YYYY-MM-DD').utc(),
         code_package: Joi.string(),
         userId: Joi.number(),
         role: Joi.string(),
     });
-    const schemaInsert = Joi.object({
-        id_product: Joi.number(),
-        id_paq: Joi.number(),
-        paq_price: Joi.number(),
-        paq_disc: Joi.string(),
-    });
-    Joi.assert(payload, schemaPackage, schemaInsert);
+    Joi.assert(payload, schema);
 }
 
 async function createPackage(req, res, next) {
@@ -43,10 +37,6 @@ async function createPackage(req, res, next) {
             date_end: packageData.date_end,
             code_package: packageData.code_package,
             user_id: userId,
-            id_product: packageData.id_product,
-            id_paq: packageData.id_paq,
-            paq_price: packageData.paq_prize,
-            paq_disc: packageData.paq_disc,
         });
         connection.release();
         res.status(201).send('New package successfully created');
